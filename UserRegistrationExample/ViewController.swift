6b39e0ab705f03c1d12833e677f52a63b8aa9725
Sameera Leola <pixelandbyte@gmail.com>
2019-08-26 23:35:50 -0500

    Initial commit

---
+++ UserRegistrationExample/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  UserRegistrationExample
+//
+//  Created by Sameera Roussi on 8/26/19.
+//  Copyright © 2019 Sameera Roussi. All rights reserved.
+//
+// Tutorial #2: https://www.youtube.com/watch?v=8ddZNw7yT9Y
+// User Sign in, Sign up and Sign out in Swift - 2. Creating Sign in User Interface
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
